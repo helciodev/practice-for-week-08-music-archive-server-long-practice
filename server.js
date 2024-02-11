31db7db9e1ca19ec434764feb8346fab7fc9f1f4
helcio <h.andre@giap.pl>
2024-02-11 13:39:40 +0100

    Complete implementing Get all albums of a specific artist based on artistId

--- server.js
+++ server.js
@@ -68,11 +68,91 @@ const server = http.createServer((req, res) => {
 
     /* ========================== ROUTE HANDLERS ========================== */
 
+    //1 Get all the artists
     if (req.method === "GET" && req.url === "/artists") {
       res.statusCode = 200;
       res.setHeader("Content-Type", "application/json");
       res.write(JSON.stringify(artists));
       return res.end();
+    }
+    //2 Get a specific artist's details based on artistId
+    if (req.method === "GET" && req.url.startsWith("/artists")) {
+      const urlSplit = req.url.split("/");
+      const artistId = urlSplit[2];
+      if (urlSplit.length === 3) {
+        const artist = artists.find((el) => el.artistId === Number(artistId));
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.write(JSON.stringify(artist));
+        return res.end();
+      }
+    }
+    //3 Add an artist
+    if (req.method === "POST" && req.url === "/artists") {
+      const { name } = req.body;
+      const newArtist = { artistId: getNewArtistId(), name };
+      artists.push(newArtist);
+
+      res.statusCode = 201;
+      res.setHeader("Content-Type", "application/json");
+      res.write(JSON.stringify(newArtist));
+      return res.end();
+    }
+    //4 Edit a specified artist by artistId
+    if (
+      req.method === "PATCH" ||
+      (req.method === "PUT" && req.url.startsWith("/artists"))
+    ) {
+      const urlSplit = req.url.split("/");
+      const artistId = urlSplit[2];
+      if (urlSplit.length === 3) {
+        const { name } = req.body;
+
+        const artist = artists.find((el) => el.artistId === Number(artistId));
+
+        artist.name = name;
+        artist.updatedAt = new Date();
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.write(JSON.stringify(artist));
+        return res.end();
+      }
+    }
+    //5 Delete a specified artist by artistId
+
+    if (req.method === "DELETE" && req.url.startsWith("/artists")) {
+      const urlSplit = req.url.split("/");
+      if (urlSplit.length === 3) {
+        const artistId = urlSplit[2];
+        const artistIndex = artists.indexOf(
+          (el) => el.artistId === Number(artistId)
+        );
+        artists.splice(artistIndex, 0);
+        res.setHeader("Content-Type", "application/json");
+        res.statusCode = 200;
+        const message = { message: "Deleted Successfully" };
+        res.write(JSON.stringify(message));
+        return res.end();
+      }
+    }
+
+    //6 Get all albums of a specific artist based on artistId
+    if (req.method === "GET" && req.url.startsWith("/artists")) {
+      const urlSplit = req.url.split("/");
+      const lastSplitted = urlSplit[3];
+      if (urlSplit.length === 4 && lastSplitted === "albums") {
+        const artistId = urlSplit[2];
+        const artistAlbums = albums.filter(
+          (el) => el.artistId === Number(artistId)
+        );
+
+        res.setHeader("Content-Type", "application/json");
+        res.statusCode = 200;
+        res.write(JSON.stringify(artistAlbums));
+        return res.end();
+      }
     }
     res.statusCode = 404;
     res.setHeader("Content-Type", "application/json");
